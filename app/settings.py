697634efdff4989841b1711a2d40bf3be35f1c44
Andy Huynh <andy.huynh312@gmail.com>
2019-08-07 23:35:59 -0700

    Get config form SSM if env variable DNE

--- app/settings.py
+++ app/settings.py
@@ -2,15 +2,27 @@ import logging
 import os
 
 import sqlalchemy
+import boto3
 
-DB_USER = os.environ["POSTGRES_USER"]
-DB_PWD = os.environ["POSTGRES_PASSWORD"]
-DB_HOST = os.environ["POSTGRES_HOST"]
-DB_PORT = os.environ["POSTGRES_PORT"]
-DB_SCHEMA = os.environ["POSTGRES_DB"]
+ssm = boto3.client('ssm')
+
+
+def _get_config(key):
+    try:
+        val = os.environ[key]
+    except KeyError:
+        val = ssm.get_parameter(Name=f'/bart-ridership/{key}', WithDecryption=True)['Parameter']['Value']
+    return val
+
+
+DB_USER = _get_config("POSTGRES_USER")
+DB_PWD = _get_config("POSTGRES_PASSWORD")
+DB_HOST = _get_config("POSTGRES_HOST")
+DB_PORT = _get_config("POSTGRES_PORT")
+DB_SCHEMA = _get_config("POSTGRES_DB")
 DB_URL = f"postgresql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_SCHEMA}"
-BART_API_TOKEN = os.environ["BART_API_TOKEN"]
-MAPBOX_ACCESS_TOKEN = os.environ["MAPBOX_ACCESS_TOKEN"]
+BART_API_TOKEN = _get_config("BART_API_TOKEN")
+MAPBOX_ACCESS_TOKEN = _get_config("MAPBOX_ACCESS_TOKEN")
 engine = sqlalchemy.create_engine(DB_URL)
 
 logging.basicConfig(
