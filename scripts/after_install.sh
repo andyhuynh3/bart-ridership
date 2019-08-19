dd8d54dd9078ca180cefa09da24501fdd89f6514
Andy Huynh <andy.huynh312@gmail.com>
2019-08-18 20:03:20 -0700

    Try getting app to deploy automatically

---
+++ scripts/after_install.sh
@@ -0,0 +1,3 @@
+service docker start
+curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
+chmod +x /opt/bin/docker-compose
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,2 @@
-service docker start
-curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
-chmod +x /opt/bin/docker-compose
-/opt/bin/docker-compose up build
+/opt/bin/docker-compose build .
 /opt/bin/docker-compose up -d > /dev/null 2> /dev/null < /dev/null &
