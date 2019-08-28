d2ed9d314425bf316f2f13c2c3460eb70416ca8a
Andy Huynh <andy.huynh312@gmail.com>
2019-08-27 23:58:05 -0700

    test ci/cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 
 
-REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 LATEST_APP_COMMIT=$(git log -n 1 --pretty=format:%H app)
 LATEST_NGINX_COMMIT=$(git log -n 1 --pretty=format:%H app)
 LATEST_DOCKERFILE_COMMIT=$(git log -n 1 --pretty=format:%H app)
@@ -11,10 +11,13 @@ if [ $TRAVIS_COMMIT = $LATEST_APP_COMMIT -o $TRAVIS_COMMIT = $LATEST_NGINX_COMMI
         echo 'Found changes to app, kicking off Docker build, tag, push'
         SOURCE_WEB_IMAGE=bart_web:latest
         TARGET_WEB_IMAGE=$REGISTRY_URL/$SOURCE_WEB_IMAGE
+        TARGET_WEB_IMAGE_COMMIT_HASH=$REGISTRY_URL/bart_web:$TRAVIS_COMMIT
         docker build ./app -t $SOURCE_WEB_IMAGE
         if [ $? -eq 0 ]; then
             docker tag $SOURCE_WEB_IMAGE $TARGET_WEB_IMAGE
             docker push $TARGET_WEB_IMAGE
+            docker tag $SOURCE_WEB_IMAGE $TARGET_WEB_IMAGE_COMMIT_HASH
+            docker push $TARGET_WEB_IMAGE_COMMIT_HASH
         else
             echo 'Building app image failed'
             exit 1
@@ -24,10 +27,13 @@ if [ $TRAVIS_COMMIT = $LATEST_APP_COMMIT -o $TRAVIS_COMMIT = $LATEST_NGINX_COMMI
         echo 'Found changes to nginx, kicking off Docker build, tag, push'
         SOURCE_NGINX_IMAGE=bart_nginx:latest
         TARGET_NGINX_IMAGE=$REGISTRY_URL/$SOURCE_NGINX_IMAGE
+        TARGET_NGINX_IMAGE_COMMIT_HASH=$REGISTRY_URL/bart_nginx:$TRAVIS_COMMIT
         docker build ./nginx -t $SOURCE_NGINX_IMAGE
         if [ $? -eq 0 ]; then
             docker tag $SOURCE_NGINX_IMAGE $TARGET_NGINX_IMAGE
             docker push $TARGET_NGINX_IMAGE
+            docker tag $SOURCE_NGINX_IMAGE $TARGET_NGINX_IMAGE_COMMIT_HASH
+            docker push $TARGET_NGINX_IMAGE_COMMIT_HASH
         else
             echo 'Building nginx image failed'
             exit 1
