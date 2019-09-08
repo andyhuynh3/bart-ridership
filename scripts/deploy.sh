a19c48c1bd6d908d48226795f639481d74043e0e
Andy Huynh <andy.huynh312@gmail.com>
2019-09-07 21:26:13 -0700

    refactoring

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,6 @@
-
-
 REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
-LATEST_APP_COMMIT=$(git log -n 1 --pretty=format:%H app)
-LATEST_NGINX_COMMIT=$(git log -n 1 --pretty=format:%H app)
-LATEST_DOCKERFILE_COMMIT=$(git log -n 1 --pretty=format:%H app)
+LATEST_APP_COMMIT=$(git log -n 1 --pretty=format:%H bart_ridership/app)
+LATEST_NGINX_COMMIT=$(git log -n 1 --pretty=format:%H bart_ridership/nginx)
 
 if [ $TRAVIS_COMMIT = $LATEST_APP_COMMIT -o $TRAVIS_COMMIT = $LATEST_NGINX_COMMIT ]; then
     $(aws ecr get-login --no-include-email)
@@ -12,7 +9,7 @@ if [ $TRAVIS_COMMIT = $LATEST_APP_COMMIT -o $TRAVIS_COMMIT = $LATEST_NGINX_COMMI
         SOURCE_WEB_IMAGE=bart_web:latest
         TARGET_WEB_IMAGE=$REGISTRY_URL/$SOURCE_WEB_IMAGE
         TARGET_WEB_IMAGE_COMMIT_HASH=$REGISTRY_URL/bart_web:$TRAVIS_COMMIT
-        docker build ./app -t $SOURCE_WEB_IMAGE
+        docker build . -f ./bart_ridership/app/Dockerfile -t $SOURCE_WEB_IMAGE
         if [ $? -eq 0 ]; then
             docker tag $SOURCE_WEB_IMAGE $TARGET_WEB_IMAGE
             docker push $TARGET_WEB_IMAGE
@@ -28,7 +25,7 @@ if [ $TRAVIS_COMMIT = $LATEST_APP_COMMIT -o $TRAVIS_COMMIT = $LATEST_NGINX_COMMI
         SOURCE_NGINX_IMAGE=bart_nginx:latest
         TARGET_NGINX_IMAGE=$REGISTRY_URL/$SOURCE_NGINX_IMAGE
         TARGET_NGINX_IMAGE_COMMIT_HASH=$REGISTRY_URL/bart_nginx:$TRAVIS_COMMIT
-        docker build ./nginx -t $SOURCE_NGINX_IMAGE
+        docker build . -f ./bart_ridership/nginx/Dockerfile -t $SOURCE_NGINX_IMAGE
         if [ $? -eq 0 ]; then
             docker tag $SOURCE_NGINX_IMAGE $TARGET_NGINX_IMAGE
             docker push $TARGET_NGINX_IMAGE
